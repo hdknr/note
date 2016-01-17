75d5ff6320fd092b4db70ad555a01ab603116308
hdknr <gmail@hdknr.com>
2016-01-17 16:08:55 +0900

    iOS Web 簡単なサンプル

---
+++ xamarin/xamarin.ios.webview.simple.cs
@@ -0,0 +1,44 @@
+void InitTiemTable()
+{
+  string contentDirectoryPath =
+    System.IO.Path.Combine (NSBundle.MainBundle.BundlePath, "");
+  string html = @"
+<html>
+<head>
+<style>
+table { width:100% }
+table td { text-align: right}
+tr:nth-child(odd) td {
+background-color: #EAF6FD;
+}
+table tr:nth-child(even) td {
+background-color: #EFEFEF;
+}
+</style>
+</head>
+<body>
+<table>
+ <thead>
+   <tr>
+     <th >時</th>
+     <th colspan='9'>分</ht>
+ </tr>
+ </thead>
+ <tbody>
+    <tr>
+       <td> 9 </td>
+       <td> 00 </td> <td> 05 </td> <td> 10 </td> <td> 15 </td> <td> 20 </td>
+       <td> 25 </td> <td> 30 </td> <td> 35 </td> <td> 40 </td>
+    </tr>
+    <tr>
+       <td> 10 </td>
+       <td> 00 </td> <td> 05 </td> <td> 10 </td> <td> 15 </td> <td> 20 </td>
+       <td> 25 </td> <td> 30 </td> <td> 35 </td> <td> 40 </td>
+    </tr>
+ </tbody>
+</table>
+<body>
+</html>";
+  TimeTable.LoadHtmlString(html,
+    new NSUrl(contentDirectoryPath, true));
+}
