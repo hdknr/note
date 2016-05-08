62cf3b0f9120e7b9d878c7274b604e938a1f6518
hdknr <gmail@hdknr.com>
2016-05-08 18:51:09 +0900

    mkdocs: markdown convert

--- docs/django/docs/views.py
+++ docs/django/docs/views.py
@@ -4,11 +4,15 @@ from django.conf import settings
 import os
 import mimetypes
 import re
+from markdown import Markdown
+import traceback
 
 
 ROOT = os.path.dirname(
     os.path.dirname(settings.BASE_DIR)).decode('utf8')
-
+MD = Markdown(
+        extensions=[u'meta', u'toc', u'tables', u'fenced_code'], 
+        extension_configs={})
 
 def _path(path):
     return os.path.join(ROOT, path)
@@ -20,5 +24,14 @@ def publish(request, path):
 
     abspath = _path(path)
     mt, dmy = mimetypes.guess_type(abspath)
-    return HttpResponse(
-        File(open(abspath)), content_type=mt)
+    mt = mt or os.path.splitext(path)[1]
+    try:
+        source = File(open(abspath))
+        if mt == ".md":
+            return HttpResponse(
+                MD.convert(source.read().decode('utf8')))
+        return HttpResponse(
+                source, content_type=mt)
+    except:
+        print traceback.format_exc()
+        return HttpResponse()
